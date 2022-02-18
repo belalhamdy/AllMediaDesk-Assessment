d04a3c286e987aa45043b694b6066a7d3372fe01
Belal Hamdy <belalhezzat@gmail.com>
2022-02-18 15:21:57 +0200

    Solved Task 2

---
+++ task_2.js
@@ -0,0 +1,57 @@
+function isNumberAscending(number) {
+    let numberStr = number.toString()
+    for (let i = 0; i < numberStr.length - 1; ++i)
+        if (numberStr[i] > numberStr[i + 1]) return false;
+    return true;
+}
+
+function runTests() {
+    let currNumber = "0";
+    for (let i = 0; i <= 1e6; ++i) {
+        if (isNumberAscending(i)) currNumber = i.toString();
+        if(solve(i.toString()) !== currNumber) {
+            alert("Error in task 2 tests.");
+            return;
+        }
+    }
+    if(solve("1000000000000000000000") !== "999999999999999999999") {
+        alert("Error in task 2 tests.");
+        return;
+    }
+    if(solve("999999999999999999998") !== "899999999999999999999") {
+        alert("Error in task 2 tests.");
+        return;
+    }
+    alert("Success");
+}
+function solve(numberStr) {
+    let numberArr = Array.from(numberStr)
+    let turning_point = numberArr.length;
+    let subtract = false;
+    for(let i = 1 ; i < numberArr.length ; ++i){
+        if(turning_point !== numberArr.length) numberArr[i] = '9';
+        else if(numberArr[i] < numberArr[i - 1]) turning_point = i;
+    }
+    let lastNonZero = 0;
+    if(turning_point !== numberArr.length){
+        for(let i = turning_point ; i >= 0 ; --i){
+            if(subtract){
+                numberArr[i] = (numberArr[i] === '0' ? '9' : numberArr[i] - '1')
+                subtract = false;
+            }
+            if(i > 0 && numberArr[i] < numberArr[i - 1]){
+                numberArr[i] = '9';
+                subtract = true;
+            }
+            if(numberArr[i] !== 0) lastNonZero = i
+        }
+    }
+
+    return numberArr.slice(lastNonZero).join("")
+}
+function run() {
+    let input = document.getElementById("task2Input").value;
+    if(input === null || input === '') console.log('bye')
+    document.getElementById("task2Result").value = solve(input);
+}
+
